19a2b53ccd726c04b2aa9edc6c86eddbc5691c20
Ivan Volkov <bahek.mel@yandex.ru>
2022-02-05 10:05:04 +0300

    FP sprint 6

---
+++ cmake-build-debug/CMakeFiles/cpp_single_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_single_linked_list.dir/single-linked-list/main.cpp.o"
+  "cpp_single_linked_list"
+  "cpp_single_linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_single_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
